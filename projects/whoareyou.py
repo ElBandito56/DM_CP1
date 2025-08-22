968fda796bb25720519e85c4611d4f0a17702189
ElBandito56 <denzel.moreno@ucas-edu.net>
2025-08-22 10:46:34 -0600

    fixing

---
+++ projects/whoareyou.py
@@ -0,0 +1,4 @@
+name = input("whats your full name?")
+age = input ("how old are you?")
+color = ("Whats your favorite color?")
+print("hello" ,name,",wow what a cool name!, what age did you say you were again?",age, )
