444ae4bead7bc248964e55ee37f1150af21cdc1e
ElBandito56 <denzel.moreno@ucas-edu.net>
2025-08-22 15:54:09 -0600

    merge branches

---
+++ projects/whoareyou.py
@@ -0,0 +1,4 @@
+name = input("whats your full name?")
+age = input ("how old are you?")
+color =input ("Whats your favorite color?")
+print("hello" ,name,",wow what a cool name!, you said you were",age,"right? and wow!" ,color,"thats my favorite too" )
